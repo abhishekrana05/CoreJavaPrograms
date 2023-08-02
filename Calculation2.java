7bd31bb41d1d7d58b81f809f47e9f288d8065da7
Abhishek Rana <44239749+abhishekrana05@users.noreply.github.com>
2023-08-02 13:36:19 +0530

    Add files via upload

---
+++ Calculation2.java
@@ -0,0 +1,16 @@
+package ControlFlowinJava;
+
+public class Calculation2 
+{
+	public static void main(String[] args) 
+	{
+		int i = 0;    
+		System.out.println("Printing the list of first 10 even numbers \n");    
+		do 
+		{    
+			System.out.println(i);    
+			i = i + 2;    
+		}while(i<=10);
+	}
+
+}
