1992d40938173d933143ed60e833f35605d4982b
Abhishek Rana <44239749+abhishekrana05@users.noreply.github.com>
2023-08-03 11:48:49 +0530

    Add files via upload

---
+++ ControlFlowinJava/Calculation1.java
@@ -0,0 +1,19 @@
+/* For-Each Loop*/
+
+package ControlFlowinJava;
+
+public class Calculation1 
+{
+
+	public static void main(String[] args)
+	{
+		String[] names = {"Java","C","C++","Python","JavaScript"};
+		System.out.println("Printing the content of the array names:\n");
+		for(String name:names)
+		{    
+			System.out.println(name);
+		} 
+
+	}
+
+}
